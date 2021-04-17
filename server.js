339d6612d9fc2e3b7090fd6d2fe81f7b42756d4b
Miguel Vilhena <mvmories@gmail.com>
2021-04-17 17:55:38 +0100

    updating database environment variable

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-transparent-16379',
-    user : 'postgres',
-    password : 'slb4ever4',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
